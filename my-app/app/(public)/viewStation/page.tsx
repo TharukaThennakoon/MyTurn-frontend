1627f5b036fcb197972f3f4219d1ec6f73725ae0
Tharuka <tmttthennakoon@std.foc.sab.ac.lk>
2026-05-25 18:25:00 +0530

    Add ViewStation component to display station details and availability

---
+++ my-app/app/(public)/viewStation/page.tsx
@@ -0,0 +1,8 @@
+export default function ViewStation() {
+  return (
+    <div style={{ padding: 24 }}>
+      <h1>View Station Page</h1>
+      <p>This is where users can view station details and availability.</p>
+    </div>
+  );
+}
